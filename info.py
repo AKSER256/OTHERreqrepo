9699fae445c319e2a2bcafbf0ec652d0da3be945
Sechu <134679332+Cybermon3g@users.noreply.github.com>
2023-11-09 21:37:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = environ.get(API_ID, "")
-API_HASH = environ.get(API_HASH, "71ed204544e0715e17ea033e0aca2d6f") 
-BOT_TOKEN = environ.get(BOT_TOKEN, "") 
+API_ID = environ.get("API_ID", "")
+API_HASH = environ.get("API_HASH", "71ed204544e0715e17ea033e0aca2d6f") 
+BOT_TOKEN = environ.get("BOT_TOKEN", "") 
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
